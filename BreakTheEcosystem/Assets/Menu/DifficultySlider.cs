df33bcabaf72667f6ade2aa2a3897e1bc045abae
Luftwaffle129 <cheeselover659@gmail.com>
2023-02-15 17:18:03 +0000

    Menu stuff

---
+++ BreakTheEcosystem/Assets/Menu/DifficultySlider.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DifficultySlider : MonoBehaviour
+{
+    public TMP_Text text;
+    public void OnDifficultyChange(float difficulty)
+    {
+        switch(difficulty)
+        {
+            case 0:
+                text.text = "EASY";
+                break;
+            case 1:
+                text.text = "HARD";
+                break;
+            case 2:
+                text.text = "EXPERT";
+                break;
+        }
+    }
+}
