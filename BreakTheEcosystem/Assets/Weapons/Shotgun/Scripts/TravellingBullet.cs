98532e289779a280cea61e1f8e69c3e4312ad81d
McLogicmaster69 <logicmaster5823@gmail.com>
2023-02-24 19:30:17 +0000

    Enemy shotgun tweaks

--- BreakTheEcosystem/Assets/Weapons/Shotgun/Scripts/TravellingBullet.cs
+++ BreakTheEcosystem/Assets/Weapons/Shotgun/Scripts/TravellingBullet.cs
@@ -1,3 +1,4 @@
+using BTE.Player;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,18 +9,32 @@ namespace BTE.Weapons
     {
         [SerializeField] private float Lifetime = 2f;
         [SerializeField] private float Speed = 30f;
+        [SerializeField] private int Damage = 3;
+        [SerializeField] private LayerMask PlayerLayer;
+
+        private Vector3 PrevPosition;
 
         private void Start()
         {
+            PrevPosition = transform.position;
             StartCoroutine(RunLife());
         }
         private void Update()
         {
+            PrevPosition = transform.position;
             transform.position += Speed * Time.deltaTime * transform.forward;
+            CheckIfHitPlayer();
         }
-        private void OnTriggerEnter(Collider other)
+        private void CheckIfHitPlayer()
         {
-            if (!other.CompareTag("Player"))
+            Vector3 directionToBullet = (PrevPosition - transform.position).normalized;
+            float distance = Vector3.Distance(transform.position, PrevPosition);
+            if(Physics.Raycast(transform.position, directionToBullet, distance, PlayerLayer))
+            {
+                PlayerHealth.main.TakeDamage(Damage);
+                Destroy(gameObject);
+            }
+            else if(Physics.Raycast(transform.position, directionToBullet, distance, PlayerLayer))
                 Destroy(gameObject);
         }
 
